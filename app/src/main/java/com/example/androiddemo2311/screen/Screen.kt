6acf20714bbc5d6b7915e7b8f63abef63b63ad1b
RAY <shyrule789@gmail.com>
2023-11-21 17:58:08 +0800

    refine pass query parameter to home screen

--- app/src/main/java/com/example/androiddemo2311/screen/Screen.kt
+++ app/src/main/java/com/example/androiddemo2311/screen/Screen.kt
@@ -2,14 +2,13 @@ package com.example.androiddemo2311.screen
 
 const val DETAIL_ARGUMENT_KEY1 = "id"
 const val DETAIL_ARGUMENT_KEY2 = "name"
-const val HOME_ARGUMENT_KEY1 = "id"
+const val HOME_ARGUMENT_KEY1 = "id_home"
 
 sealed class Screen(val route: String) {
-    object Home: Screen(route = "home_screen?id={id}") {
-        fun passId(id: Int = 0): String = "home_screen?id=$id"
+    object Home: Screen(route = "home_screen?$HOME_ARGUMENT_KEY1={$HOME_ARGUMENT_KEY1}") { // 前後參數內容要一致
+        fun passId(id: Int = 0): String = "home_screen?$HOME_ARGUMENT_KEY1=$id"
     }
     object Detail: Screen(route = "detail_screen/{$DETAIL_ARGUMENT_KEY1}/{$DETAIL_ARGUMENT_KEY2}") { // '{}' for required args
-        //fun passId(id: Int): String = this.route.replace(oldValue = "{$DETAIL_ARGUMENT_KEY1}", newValue = "$id")
         fun passIdAndName(id: Int, name: String): String = "detail_screen/$id/$name"
 
     }
